3f0238b2099abee31cae361e3d1099bd56fc5a17
Alfred Mueller <aj.mueller@hotmail.com>
2025-01-21 19:44:59 +0100

    Deploy Test

--- api/main.py
+++ api/main.py
@@ -190,3 +190,5 @@ def get_games_by_developer(developer_id: int):
         return games
     except DeveloperNotFoundError:
         raise HTTPException(detail="Developer not found", status_code=404)
+
+# Deploy Test
